41551d87e923f82d93f974187e4cf7bc2b1b9753
Tallulah Andrews <ta6@sanger.ac.uk>
2016-11-03 11:22:18 +0000

    Salmon and Kallisto build index update

--- 0.2_Salmon_build_index.sh
+++ 0.2_Salmon_build_index.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# Raw genome fasta and annotation gtf
+# Raw genome fasta and annotation gtf files
 FA=/lustre/scratch108/compgen/team218/TA/genomebuilding/Mus_musculus.GRCm38.dna.primary_assembly.fa
 GTF=/lustre/scratch108/compgen/team218/TA/genomebuilding/Mus_musculus.GRCm38.79.gtf
 
-# Location for output files
+# Location for output
 OUTDIR=/lustre/scratch108/compgen/team218/TA/STRIPED_GENOMES
 
-# Locations of required software
+# Location of required software
 GFFREAD=/nfs/users/nfs_t/ta6/RNASeqPipeline/software/cufflinks-2.2.1.Linux_x86_64/gffread
-KALLISTO=/nfs/users/nfs_t/ta6/RNASeqPipeline/software/kallisto_linux-v0.42.4/kallisto
+SALMON=/nfs/users/nfs_t/ta6/RNASeqPipeline/software/Salmon-0.7.2_linux_x86_64/bin/salmon 
 
-# Extract transcriptome fasta using gffread
+# Extract transcriptome fasta
 $GFFREAD $GTF -g $FA -w $OUTDIR/Transcripts.fasta
 
-# Index the extracted transcriptome
-$KALLISTO index -i $OUTDIR/kallisto_index.idx $OUTDIR/Transcripts.fasta
+# Build index (single thread)
+$SALMON index -i $OUTDIR/salmon_index -t $OUTDIR/Transcripts.fasta -p 1
 
