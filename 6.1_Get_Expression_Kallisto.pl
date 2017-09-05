cf2693e8be19783611e610ccc445e5320fd0d935
Tallulah Andrews <ta6@sanger.ac.uk>
2017-09-05 10:57:19 +0100

    More user-friendly Kallisto matrix maker

--- 6.1_Get_Expression_Kallisto.pl
+++ 6.1_Get_Expression_Kallisto.pl
@@ -1,16 +1,25 @@
 use strict;
 use warnings;
 
-if (@ARGV != 1) {die "Please provide a /path/prefix for output.\n";}
+if (@ARGV != 4) {die "Usage: perl 6.1_Get_Expression_Kallisto.pl /path/outprefix /path/genome.gtf /path/kallisto_output_directory [0/1 - aggregate by gene?].\n";}
 
 my $outprefix = $ARGV[0];
+my $gtf_file = $ARGV[1];
+my $abundance_string = $ARGV[2]."*.abundance*.tsv";
+my $agg_by_gene = $ARGV[3];
+
+my $suffix = "kallisto_trans";
+if ($agg_by_gene) {
+	$suffix = "kallisto_gene";
+}
 
 my %Gene2ID2FragCount = ();
 my %Gene2ID2TPM = ();
 my @IDs = ();
 
 my %transcript2gene = ();
-open (my $ifh, "/lustre/scratch108/compgen/team218/TA/genomebuilding/Mus_musculus.GRCm38.79.gtf") or die $!;
+#open (my $ifh, "/lustre/scratch108/compgen/team218/TA/genomebuilding/Mus_musculus.GRCm38.79.gtf") or die $!;
+open (my $ifh, "$gtf_file") or die $!;
 while (<$ifh>) {
 	if ($_ =~ /^#/) {next;}
 	my $geneid = "ERROR";
@@ -32,10 +41,12 @@ while (<$ifh>) {
 } close ($ifh);
 print STDERR "Done reading Annotations\n";
 
-foreach my $file (glob("/lustre/scratch108/compgen/team218/TA/RNASeqFilesQuantified/Buettner_Kallisto/*.abundance*.tsv")) {
+#foreach my $file (glob("/lustre/scratch108/compgen/team218/TA/RNASeqFilesQuantified/Buettner_Kallisto/*.abundance*.tsv")) {
+foreach my $file (glob("$abundance_string")) {
 #	$file =~ /([ATCG]{5,})/;
 	my $ID = "ERROR";
-	if ($file =~ /([^\/]+_Cell\d\d)/) {
+#	if ($file =~ /([^\/]+_Cell\d\d)/) {
+	if ($file =~ /([^\/]+)\.abundance/) {
 		$ID = $1;
 	} else {die "$file did not match!\n";}
 	push(@IDs,$ID);
@@ -45,9 +56,9 @@ foreach my $file (glob("/lustre/scratch108/compgen/team218/TA/RNASeqFilesQuantif
 		if ($_ =~ /^#/ || $_ =~ /^target_id/) {next;} #skip header & comments
 		my @record=split(/\t/);
 		my $gene = $record[0]; $gene =~ s/\s+//g;
-#		if (exists($transcript2gene{$gene})) {
-#			$gene = $transcript2gene{$gene};
-#		}
+		if (exists($transcript2gene{$gene}) && $agg_by_gene) {
+			$gene = $transcript2gene{$gene};
+		}
 		if (exists($Gene2ID2FragCount{$gene}->{$ID})) {
 			$Gene2ID2FragCount{$gene}->{$ID} += $record[3]; 
 			$Gene2ID2TPM{$gene}->{$ID} += $record[4]; 
@@ -60,8 +71,8 @@ foreach my $file (glob("/lustre/scratch108/compgen/team218/TA/RNASeqFilesQuantif
 
 
 
-open(my $ofh1, ">", "$outprefix\_kallisto_counts.txt") or die $!;
-open(my $ofh2, ">", "$outprefix\_kallisto_tpm.txt") or die $!;
+open(my $ofh1, ">", "$outprefix\_$suffix\_counts.txt") or die $!;
+open(my $ofh2, ">", "$outprefix\_$suffix\_tpm.txt") or die $!;
 print $ofh1 "Gene\t".join("\t",@IDs)."\n";
 print $ofh2 "Gene\t".join("\t",@IDs)."\n";
 foreach my $gene (keys(%Gene2ID2FragCount)) {
